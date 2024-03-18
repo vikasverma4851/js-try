87191dfd51d2476b61b7feae7947891d43aaacb5
Vikas Verma <vikasverma4851@gmail.com>
2024-03-18 22:47:36 +0530

    dataTypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+//alert (3+3) // we are using nodejs, not browser
+
+console.log(3+3);
+
+console.log("Vikas Verma");
+
+let name = "Vikas"
+let age = 23 
+let isLoggedIn = false
+let state;
+
+//number => 1 to power 53
+//bigint 
+//string => ""
+//boolean => true/fase
+//null => standalone value
+//undefined =>
+//symbol  => unique data type
+
+//object 
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
