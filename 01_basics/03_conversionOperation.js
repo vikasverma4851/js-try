76e15abc9d73fb7af6dbd10b386336e762be1e9c
Vikas Verma <vikasverma4851@gmail.com>
2024-03-18 23:08:18 +0530

    conversionOperation on datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = undefined;
+
+console.log(typeof score);
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = "Vikas"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); 
+
+// 1 => true; 0 => false
+//"" => false
+//"Vikas" = true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber); // "33"
+console.log( typeof stringNumber); // "33"
