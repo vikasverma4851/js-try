c598c6d1a0bacf3734b6fb30d33b02eedd132ac7
Vikas Verma <vikasverma4851@gmail.com>
2024-03-19 12:45:01 +0530

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+//Primitive
+
+// 7 types : String,Number,Boolean, null ,undefined, Symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 43243547657868998n
+
+
+
+//Reference  (Non-primitive)
+
+//Array , Objects ,Functions 
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let myObj = {
+    name:"vikas",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof heros);
