351d34de170849d229e11a3b64f3ecb4f9632948
Vikas Verma <vikasverma4851@gmail.com>
2024-03-19 11:39:18 +0530

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = undefined;
 
-console.log(typeof score);
-console.log(typeof score);
+// console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score);
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
 //"33" => 33
 // "33abc" => NaN
@@ -14,7 +14,7 @@ console.log(valueInNumber);
 let isLoggedIn = "Vikas"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn); 
+//console.log(booleanIsLoggedIn); 
 
 // 1 => true; 0 => false
 //"" => false
@@ -23,5 +23,41 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber);
-console.log(stringNumber); // "33"
-console.log( typeof stringNumber); // "33"
+// console.log(stringNumber); // "33"
+// console.log( typeof stringNumber); // "33"
+
+
+//***************** Operation  ****************//
+
+let value = 3 
+let negValue =-value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = "Vikas"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1+ 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1 ,num2, num3
+
+num1 = num2 = num3 =2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
